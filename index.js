109c60b3564d6897bfd5930bb7f049ba14476fc8
Warren Zhang <warrenzhang17@gmail.com>
2023-03-21 11:25:21 -0400

    distance lab

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    const hq = 42; 
+    if (someValue < 0) {
+      return "Invalid block number"; 
+    }
+    return Math.abs(hq - someValue); 
+  }
+
+function distanceFromHqInFeet(someValue) {
+    const blockLength = 264; // value provided in indexText.js
+    const blocks = distanceFromHqInBlocks(someValue);
+
+    return blocks * blockLength; 
+}
+
+function distanceTravelledInFeet(start, destination) {
+    const blockLength = 264;
+    const distanceInBlocks = Math.abs(destination - start);
+    const distanceInFeet = distanceInBlocks * blockLength;
+
+    return distanceInFeet
+}
+
+function calculatesFarePrice(start, destination) {
+    const distanceInFeet = distanceTravelledInFeet(start, destination);
+    let farePrice = 0;
+    if (distanceInFeet <= 400) {
+        farePrice = 0;
+    } else if (distanceInFeet <= 2000) {
+        farePrice = (distanceInFeet - 400) * 0.02;
+    } else if (distanceInFeet <= 2500) {
+        farePrice = 25;
+    } else {
+        farePrice = "cannot travel that far";
+    }
+    return farePrice;
+}
